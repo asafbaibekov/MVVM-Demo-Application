1e54ce586dc2e9f90b94b65be3aac31e98bf9b83
Asaf Baibekov <asafb@gini-apps.com>
2022-07-31 11:16:54 +0300

    Initial Commit

---
+++ MVVM Demo Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Demo Application
+//
+//  Created by Asaf Baibekov on 31/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
