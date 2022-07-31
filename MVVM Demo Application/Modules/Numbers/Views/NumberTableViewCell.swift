9d31b470b0dc799f56d7aa2c025f0e8f1192589c
Asaf Baibekov <asafb@gini-apps.com>
2022-07-31 14:59:08 +0300

    create table view controller for the numbers screen

---
+++ MVVM Demo Application/Modules/Numbers/Views/NumberTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  NumberTableViewCell.swift
+//  MVVM Demo Application
+//
+//  Created by Asaf Baibekov on 31/07/2022.
+//
+
+import UIKit
+
+class NumberTableViewCell: UITableViewCell {
+
+	@IBOutlet private var numberLabel: UILabel!
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		// Initialization code
+	}
+
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
+		// Configure the view for the selected state
+
+	}
+	
+}
