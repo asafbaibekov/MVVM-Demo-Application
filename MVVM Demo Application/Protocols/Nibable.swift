3de8a8a62ae5db4324f0622411b180c94d7eab6c
Asaf Baibekov <asafb@gini-apps.com>
2022-07-31 15:26:48 +0300

    add default implementation for instantiate for UIView and add nib computed property to Nibable protocol

--- MVVM Demo Application/Protocols/Nibable.swift
+++ MVVM Demo Application/Protocols/Nibable.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 protocol Nibable {
+
+	static var nib: UINib { get }
+
 	static func instantiate() -> Self
+}
+
+extension Nibable {
+	static var nib: UINib { UINib(nibName: String(describing: self), bundle: .main) }
+}
+
+extension Nibable where Self: UIView {
+	static func instantiate() -> Self {
+		return nib.instantiate(withOwner: self).first as! Self
+	}
 }
 
 extension Nibable where Self: UIViewController {
